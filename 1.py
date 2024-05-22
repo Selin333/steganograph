c75b0f4e54255c40bd75596c6e4fb46030300a06
Selin333 <94107697+Selin333@users.noreply.github.com>
2024-05-22 20:32:35 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 import docx
 import MTK2
-from docx.shared import Pt, RGBColor
+from docx.shared import RGBColor
 from docx.enum.text import WD_COLOR_INDEX
 
-doc = docx.Document('C:\\Users\\Gdl\\Desktop\\ВКБ\\Сафарьян\\2\\2\\scrito.docx')
+doc = docx.Document("C:\\Users\\Godless\\Desktop\\ВКБ\\Сафарьян\\1\\variant10 (1).docx")
 
 
 def run_get_spacing(run):
@@ -19,6 +19,7 @@ def run_get_scale(run):
 
 
 def main():
+    prov = False
     kod = ''
     for paragraph in doc.paragraphs:
         for run in paragraph.runs:
@@ -27,9 +28,8 @@ def main():
             font_highlight_color = run.font.highlight_color
             font_scale = run_get_scale(run)
             font_spacing = run_get_spacing(run)
-
             if (font_color != RGBColor(0, 0, 0) or
-                    font_size.pt != 15.0 or
+                    font_size.pt != 12.0 or
                     font_highlight_color != WD_COLOR_INDEX.WHITE or
                     font_spacing or font_scale):
                 for i in range(len(run.text)):
@@ -38,21 +38,38 @@ def main():
                 for i in range(len(run.text)):
                     kod += '0'
 
+            if prov == False:
+                if font_color != RGBColor(0, 0, 0):
+                    print('Закодированно цветом')
+                    prov = True
+                if font_size.pt != 12.0:
+                    print('Закодированно размером шрифта')
+                    prov = True
 
-    #for paragraph in doc.paragraphs:
-     #   for run in paragraph.runs:
-     #       print(run.text)
-    def decode_to_string(binary_string):
-        
-        unicode_chars = [binary_string[i:i + 16] for i in range(0, len(binary_string), 16)]
-        decoded_message = ''.join([chr(int(char, 2)) for char in unicode_chars])
-        return decoded_message
+                if font_highlight_color != WD_COLOR_INDEX.WHITE:
+                    print('Закодированно цветом фона')
+                    prov = True
+
+                if font_spacing or font_scale:
+                    print('Закодированно масштабом')
+                    prov = True
+        print(paragraph.text)
+
+
+    
+    while len(kod)%8!=0:
+        kod += "0"
+    print('Код скрытого текста:')
     print(kod)
-
-    decoded_message = decode_to_string(kod)
-    tttt = decoded_message.find('.') +1
-    print("Декодированное сообщение:", decoded_message[:tttt])
-
+    # print(len(kod))
+    normaltext = bytes.fromhex(hex(int(kod, 2))[2:]).decode(encoding="koi8_r")
+    print('Перевод в кодировку koi8 - ',normaltext[:normaltext.find('.')+1])
+    normaltext = bytes.fromhex(hex(int(kod, 2))[2:]).decode(encoding="cp866")
+    print('Перевод в кодировку cp866 - ',normaltext[:normaltext.find('.')+1])
+    normaltext = bytes.fromhex(hex(int(kod, 2))[2:]).decode(encoding="cp1251")
+    print('Перевод в кодировку cp1251 - ',normaltext[:normaltext.find('.')+1])
+    normaltext = MTK2.MTK2_decode(kod)
+    print('Перевод в кодировку MTK2 - ',normaltext)
 
 
 if __name__ == '__main__':
